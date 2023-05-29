cfcf68af2b91b948b966c4f0c502ff0f6a3530dc
Nathantambourine <340867852@gapps.yrdsb.ca>
2023-05-29 15:20:19 -0400

    Test

--- signout.py
+++ signout.py
@@ -1,2 +1,17 @@
+import tkinter as tk 
+
+root = tk.Tk()
+
+root.geometry("300x300")
+root.title("Test")
+
+label = tk.Label(root, text="Hello World!", font=('Arial', 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, font=('Arial',22))
+textbox.pack()
+
+root.mainloop()
+
 email = input("What is your gapps email?:")
 name = input("What is your full name?:")
