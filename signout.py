7112f7418bfeb6ba560a2299901f4f01f08475db
ashish345678 <350236378@gapps.yrdsb.ca>
2023-05-30 15:18:59 -0400

    nathan eats burgers every day

--- signout.py
+++ signout.py
@@ -1,10 +1,17 @@
 from tkinter import*
+from tkinter import ttk
 
 root = Tk()
+root.geometry("700x700")
+root.title("camera sign out sheet")
+
 
 rebel = 3
 nikon = 3
-SignOut = Button(root, text="Sign out" )
-SignOut.pack()
+SignOut = ttk.Button(root, text="Sign out", height = 5, width = 15).pack()
+
+
+
+
 
 root.mainloop()
