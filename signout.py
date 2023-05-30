3bf8364b7ad5c41a98737d84f8f4be8be1933617
ashish345678 <350236378@gapps.yrdsb.ca>
2023-05-30 14:47:09 -0400

    commit 2

--- signout.py
+++ signout.py
@@ -2,16 +2,6 @@ import tkinter as tk
 
 root = tk.Tk()
 
-root.geometry("300x300")
-root.title("Test")
-
-label = tk.Label(root, text="Hello World!", font=('Arial', 18))
-label.pack(padx=20, pady=20)
-
-textbox = tk.Text(root, font=('Arial',22))
-textbox.pack()
+SignOut = Button(root, text="Sign out" )
 
 root.mainloop()
-
-email = input("What is your gapps email?:")
-name = input("What is your full name?:")
