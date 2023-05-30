824be72a3df1de892d5c55fa825b2477f31e56b6
ashish345678 <350236378@gapps.yrdsb.ca>
2023-05-30 15:30:50 -0400

    nathan eats healthy food every 2 months

--- signout.py
+++ signout.py
@@ -8,11 +8,8 @@ root.title("camera sign out sheet")
 
 rebel = 3
 nikon = 3
-SignOut = Button(root, text="Sign out", height = 5, width = 15).pack()
-
-
-
-
+SignOut = Button(root, text="Sign out", height = 5, width = 15)
+SignOut.place(x = 280, y = 300)
 
 
 root.mainloop()
