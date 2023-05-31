6cf7c6368e2a58e66ae7b0de9268fc09833ed076
metal-locker <340900554@gapps.yrdsb.ca>
2023-05-31 14:58:28 -0400

    Update signout.py

--- signout.py
+++ signout.py
@@ -1,28 +1,49 @@
-from tkinter import*
+from tkinter import *
+from tkinter.ttk import *
 
 
 root = Tk()
 root.geometry("700x700")
 root.title("camera sign out sheet")
 
+def signoutWindow():
+    # Opens the window that is for signing out cameras
+    newWindow = Toplevel(root)          
+    newWindow.geometry("700x700")       # Sets the geometry of the new window
+    Label(newWindow, text="This page is for signing out cameras").pack()      # Packs the new window and gives it a label
+
+def returnWindow():
+    # Opens the window that is for returning cameras
+    newWindow = Toplevel(root)          
+    newWindow.geometry("700x700")       # Sets the geometry of the new window
+    Label(newWindow, text="This page is for returning cameras").pack()      # Packs the new window and gives it a label
+
+def reportWindow():
+    # Opens the window that is for reporting damaged cameras
+    newWindow = Toplevel(root)          
+    newWindow.geometry("700x700")       # Sets the geometry of the new window
+    Label(newWindow, text="This page is for reporting damaged cameras").pack()      # Packs the new window and gives it a label
 
 rebel = 3
 nikon = 3
-SignOut = Button(root, text="Sign out", height = 5, width = 15)
+SignOut = Button(root, text="Sign out", command=signoutWindow)
 SignOut.place(x = 280, y = 300)
 
 
-Return = Button(root, text="return", height = 5, width = 15)
+Return = Button(root, text="return", command=returnWindow)
 Return.place(x = 100, y = 300)
 
-Report = Button(root, text="report", height = 5, width = 15)
+Report = Button(root, text="report", command=reportWindow)
 Report.place(x = 450, y = 300)
 
 
 root.mainloop()
 
 def signout():
+    pass
 
 def returncamera():
+    pass
 
 def report():
+    pass
