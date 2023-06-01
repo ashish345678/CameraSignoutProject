f3d6fc978478e65b849fd74409db2bfc6f8e250f
metal-locker <340900554@gapps.yrdsb.ca>
2023-06-01 14:37:21 -0400

    colour

--- signout.py
+++ signout.py
@@ -5,7 +5,7 @@ from tkinter.ttk import*
 root = Tk()
 root.geometry("700x700")
 root.title("camera sign out sheet")
-label = Label(root, text="Camera Sign Out", bg="beige")
+label = Label(root, text="Camera Sign Out")
 root.configure(bg="beige")
 
 def signoutWindow():
@@ -13,21 +13,21 @@ def signoutWindow():
     newWindow = Toplevel(root)          
     newWindow.geometry("700x700")       # Sets the geometry of the new window
     newWindow.configure(bg="beige")
-    Label(newWindow, text="This page is for signing out cameras", bg="beige").pack()      # Packs the new window and gives it a label
+    Label(newWindow, text="This page is for signing out cameras").pack()      # Packs the new window and gives it a label
 
 def returnWindow():
     # Opens the window that is for returning cameras
     newWindow = Toplevel(root)          
     newWindow.geometry("700x700")       # Sets the geometry of the new window
     newWindow.configure(bg="beige")
-    Label(newWindow, text="This page is for returning cameras", bg="beige").pack()      # Packs the new window and gives it a label
+    Label(newWindow, text="This page is for returning cameras").pack()      # Packs the new window and gives it a label
 
 def reportWindow():
     # Opens the window that is for reporting damaged cameras
     newWindow = Toplevel(root)          
     newWindow.geometry("700x700")       # Sets the geometry of the new window
     newWindow.configure(bg="beige")
-    Label(newWindow, text="This page is for reporting damaged cameras", bg="beige").pack()      # Packs the new window and gives it a label
+    Label(newWindow, text="This page is for reporting damaged cameras").pack()      # Packs the new window and gives it a label
 
 rebel = 3
 nikon = 3
